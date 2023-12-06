9b2e3790358a7178538ce08d3d55eaf0e311729e
primitvd <davidezhanikat@gmail.com>
2023-12-06 23:09:08 +0530

    It is done

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import  random
 
 def give_card():
-    cards = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-    return random.choice(cards)
+    set_cards = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+    return random.choice(set_cards)
 
 def score_check(cards):
     score = 0
+    set_cards = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
     for card in cards:
-        if card == "J" or cards == "Q" or cards == "K":
+        if card == "J" or card == "Q" or card == "K":
             score += 10
-        if card == "A":
+            print("a",score)
+        elif card == "A":
             score += 11
+            print("b",score)
         else:
-            score += cards.index(card) + 1
-    if score > 21 and "A" in cards:
-        score -= 10
+            score += set_cards.index(card) + 1
+            print("c",score)
+    for card in cards:
+        if score > 21 and card == "A":
+            score -= 10
     return score
 
 
@@ -29,6 +34,9 @@ while input("Type y to get another card, type n to pass: ") == "y":
     player.append(give_card())
     print("Your Cards:",player)
     print("Computer's first card:",computer[0])
+    if score_check(player) > 21:
+        print("You lose")
+        break
 
 print("Your final hand:",player)
 print("Computer's final hand:",computer)
@@ -36,10 +44,13 @@ print("Computer's final hand:",computer)
 player_score = score_check(player)
 computer_score = score_check(computer)
 
-if player_score > 21 or player_score < computer_score:
+print("Player score:",player_score)
+print("Computer Score:",computer_score)
+
+if player_score > 21 or (player_score < computer_score and computer_score <= 21):
     print("You lose")
 
-elif computer_score < player_score and player_score <= 21:
+elif (computer_score < player_score and player_score <= 21) or computer_score > 21:
     print("You win")
 
 else:
